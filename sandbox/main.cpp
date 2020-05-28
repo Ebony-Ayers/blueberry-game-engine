cde88f4ebdfc862f7aab738fdae4f3a82a4ace9d
Joseph Davies <daviesjd1@gmail.com>
2020-05-28 19:27:22 +1000

    Set up repo folder structure

---
+++ sandbox/main.cpp
@@ -0,0 +1,9 @@
+//Joseph Davies 2020
+//https://github.com/Joseph-Davies/
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Hello World\n";
+	
+	return 0;
+}
---
+++ src/BlueBerry.cpp
@@ -0,0 +1,3 @@
+//Joseph Davies 2020
+//https://github.com/Joseph-Davies/
+
