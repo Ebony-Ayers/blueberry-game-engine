70f263b62209da0ceb4a9db3ecaf6a7107fa6fd9
Joseph Davies <daviesjd1@gmail.com>
2020-06-03 18:19:42 +1000

    set up cmake

--- src/BlueBerry.cpp
+++ src/BlueBerry.cpp
@@ -1,25 +1,31 @@
 //Joseph Davies 2020
 //https://github.com/Joseph-Davies/
 
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
-//#include <GLFW/glfw3.h>
-#include <iostream>
+#include <pch.h>
 
 namespace BlueBerry
 {
 	void init()
 	{
 		std::cout << "Initialising Engine\n";
-		/*
+		
 		GLFWwindow* window;
 		glfwInit();
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-		window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+		window = glfwCreateWindow(800, 600, "Blue Berry game engine test", nullptr, nullptr);
 		while(!glfwWindowShouldClose(window))
 		{
 			glfwPollEvents();
 		}
 		glfwTerminate();
-		*/
+		
 	}
 }
---
+++ src/pch.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+
+#include <math.h>
+
+#include <string>
+#include <cstring>
+
+#include <memory>
+
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+
+#include <algorithm>
+
+#include <stdexcept>
+
+#include <cstdlib>
+#include <cstdint>
+
+#include <optional>
+
+#include <chrono>
+
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+#include <future>
