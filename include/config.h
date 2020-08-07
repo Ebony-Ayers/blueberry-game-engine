95b82bafd479d1ba13d61a080cfea70cbb8746b7
Joseph Davies <daviesjd1@gmail.com>
2020-08-07 15:33:23 +1000

    changed how the engine handels compile macros

---
+++ include/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef BB_CMAKE
+	#ifndef BB_DEBUG
+		#define BB_DEBUG true
+	#endif
+	#ifndef BB_RELEASE
+		#define BB_RELEASE false
+	#endif
+	#ifndef BB_PROFILING
+		#define BB_PROFILING false
+	#endif
+#endif
